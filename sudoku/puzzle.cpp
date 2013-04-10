9dff486ae2685aa85aa38c6366d0fdc16e59422b
Nathan Lin <natlin@ucdavis.edu>
2013-04-09 23:48:44 -0700

    Started Puzzle.cpp by getting making a list of all the values that [x][y] cannot be and storing it in gridCount

--- sudoku/puzzle.cpp
+++ sudoku/puzzle.cpp
@@ -1,11 +1,72 @@
 //Authors Nathan Lin, Vijay Kumar
 #include "puzzle.h"
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 Puzzle::Puzzle(const char grid[][9])
 {
+  int i, j, k;
+  int x, y, z;
+  vector<int> temp;
+  char insert;
+  temp.clear();
+  for(i = 0; i < 9; i++)
+  {
+    for(j = 0; j < 9; j++)
+    {
+      if(grid[i][j] == '*')
+      {
+        for(k = 0; k < 9; k++)
+        {
+          if(grid[i][k] != '*')
+          {
+            insert = grid[i][k];
+            //cout << insert << " " << static_cast<int>(insert) - 48 << ", " ;
+            temp.push_back(static_cast<int>(insert) - 48);
+          }//if
+        }//for(k)
+        for(k = 0; k < 9; k++)
+        {
+          if(grid[k][j] != '*')
+          {
+            insert = grid[k][j];
+            //cout << insert << " " << static_cast<int>(insert) - 48 << ", " ;
+            temp.push_back(static_cast<int>(insert) - 48);
+          }//if
+        }//for(k)
+        vector<int>::iterator it;
+        for(x = 0; x < static_cast<int>(temp.size()); x++)
+        {
+          it = temp.begin();
+          for(y = x + 1; y < static_cast<int>(temp.size()); y++)
+          {
+            if(temp.at(x) == temp.at(y))
+            {
+              for(z = 0; z < y; z++)
+                it++;
+              temp.erase(it);
+            }//if
+          }//for(y)
+        }//for(x)
+      }//if
+      else
+      {
+        temp.push_back(0);
+        gridCount[i][j]=temp;
+      }//else
+      //for(vector<int>::iterator b = temp.begin(); b != temp.end(); b++)
+        //cout << *b << " ";
+      gridCount[i][j]=temp;
+      temp.clear();
+    }//for(j)
+  }//for(i)
+  for(vector<int>::iterator b = gridCount[0][0].begin(); b != gridCount[0][0].end(); b++)
+    cout<< *b << ' ';
 }//Puzzle()
 
 void Puzzle::solve(char grid[][9])
 {
+  
 }//solve()
--- sudoku/puzzle.h
+++ sudoku/puzzle.h
@@ -1,11 +1,17 @@
 #ifndef PUZZLE_H
   #define PUZZLE_H
 
+#include <vector>
+using namespace std;
+
 class Puzzle
 {
  public:
-  Puzzle(const char grid[][9]) {}
-  void solve(char grid[][9]) {}
+  Puzzle(const char grid[][9]);
+  void solve(char grid[][9]);
+ private:
+  vector<int> testNum;
+  vector<int> gridCount[9][9];
 }; // Puzzle class
 
 #endif
